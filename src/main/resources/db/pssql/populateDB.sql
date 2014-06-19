803f5c24ab818974dd250c24642d4bdd086b14c0
Laurent Stemmer <laurent.stemmer@monsieurdrive.com>
2014-06-19 21:32:13 +0200

    sql

--- src/main/resources/db/pssql/populateDB.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO sites(id, name, host, username, password, log_path, log_type, connection_type) VALUES (1, 'SFR', '127.0.0.1', 'sfr_site', 'azerty', '/home/sfr_site/logs/sfrlogs.csv', 'CSV', 'FTP');
